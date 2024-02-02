d6dd464c2cfea9ddccc618ddffb961c29519b94c
al-kuznetsov <alkuznetsov@aol.com>
2024-02-02 16:42:05 +0700

    Add simple remote control example

--- src/command/simpleremote/RemoteControlTest.java
+++ src/command/simpleremote/RemoteControlTest.java
@@ -1,16 +1,18 @@
-package command;
+package command.simpleremote;
 
+// Это клиент, которые создает команду и назначает исполнителя
 public class RemoteControlTest {
 
 	public static void main(String[] args) {
 		Light light = new Light(); // Получатель команды/запроса
 		LightOnCommand lightOnCommand = new LightOnCommand(light); // Команда, с указанием получателя
 
-		SimpleRemoteControl remote = new SimpleRemoteControl(); // Инициатор запроса
+		SimpleRemoteControl remote = new SimpleRemoteControl(); // Инициатор запроса (Invoker)
 
 		remote.setCommand(lightOnCommand); // На кнопку назначена команда
 		remote.buttonWasPressed(); // Инициировано выполнение команды получателем
 
+		// Исполнители запроса (Receiver)
 		GarageDoor garageDoor = new GarageDoor();
 		GarageDoorOpenCommand garageDoorOpenCommand = new GarageDoorOpenCommand(garageDoor);
 
